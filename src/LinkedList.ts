00b4798345264a655d5cb36a6341f92eab528a94
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-21 11:14:21 +0200

    finish implementation of LinkedList and sort on this

--- src/LinkedList.ts
+++ src/LinkedList.ts
@@ -72,7 +72,7 @@ export class LinkedList implements Sortable {
 
 		let node: Node | null = this.head;
 		while (node) {
-			console.log(node);
+			console.log(node.data);
 			node = node.next;
 		}
 	}
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { NumberCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 const numberCollection = new NumberCollection([11, 3, -4, 1]);
 const sorter = new Sorter(numberCollection);
@@ -11,3 +12,13 @@ const strings = new CharactersCollection('xdfca');
 const sorterString = new Sorter(strings);
 sorterString.sort();
 console.log(strings.data);
+
+const linkedList = new LinkedList();
+linkedList.add(200);
+linkedList.add(20);
+linkedList.add(100);
+linkedList.add(-200);
+
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
