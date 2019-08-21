478679833069c917e4992efa715d3ee589e872c1
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-21 10:21:12 +0200

    Add abilty to sort strings

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { NumberCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
+import { CharactersCollection } from './CharactersCollection';
 
 const numberCollection = new NumberCollection([11, 3, -4, 1]);
 const sorter = new Sorter(numberCollection);
 sorter.sort();
 console.log(numberCollection.data);
+
+const strings = new CharactersCollection('xdfca');
+const sorterString = new Sorter(strings);
+sorterString.sort();
+console.log(strings.data);
