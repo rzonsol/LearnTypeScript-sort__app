29c205d422b03763e45e8eb332734b26ec9aebdf
przonsow <piotr.rzonsowski@cognitran.com>
2019-08-21 08:31:05 +0200

    first implementation of sorting algorithm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hi Piotr :)');
+class Sorter {
+	constructor(public collection: number[]) {
+		this.collection = collection;
+	}
+
+	sort(): void {
+		const { length } = this.collection;
+		for (let i = 0; i < length; i++) {
+			for (let j = 0; j < length - i - 1; j++) {
+				if (this.collection[j] > this.collection[j + 1]) {
+					const temp = this.collection[j];
+					this.collection[j] = this.collection[j + 1];
+					this.collection[j + 1] = temp;
+				}
+			}
+		}
+	}
+}
+
+const sorter = new Sorter([11, 3, -4, 1]);
+sorter.sort();
+console.log(sorter.collection);
